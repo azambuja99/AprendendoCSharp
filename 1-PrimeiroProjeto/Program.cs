70498280540e2146bf47b4d40e128b6546b5134e
ferna <ferna@FERNANDO-DEV>
2018-05-06 23:30:22 -0400

    Add project files.

---
+++ 1-PrimeiroProjeto/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _1_PrimeiroProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\nOlá, Mundo. Projeto no Visual Studio!");
+
+            Console.WriteLine("A execusão acabou. Tecle enter para finalizar...");
+
+            Console.ReadLine();
+        }
+    }
+}
