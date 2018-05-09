c4f5e85a74fa77f359063dd11b8fe477087e6d0e
ferna <ferna@FERNANDO-DEV>
2018-05-08 20:30:11 -0400

    testes

---
+++ 4-Conversoeseoutrostiposnumericos/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _4_Conversoeseoutrostiposnumericos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 4");
+
+            double salario;
+            salario = 1200.50;
+
+            int salarioEmInteiro;
+            salarioEmInteiro = (int)salario;
+
+            Console.WriteLine(salarioEmInteiro);
+
+
+            Console.WriteLine("Fim Da Execução...............");
+            Console.ReadLine();
+        }
+    }
+}
