45d953046b5910482161ed91118e9171aa8804ee
ferna <ferna@FERNANDO-DEV>
2018-05-17 21:37:44 -0400

    fim primeiro projeto

---
+++ 10-CalculaPoupanca/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _10_CalculaPoupanca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 10 - Calcula Poupança");
+
+            double valorInvestido = 1000;
+
+            //0.36% = 0.0036
+            //valorInvestido = valorInvestido + valorInvestido * 0.0036;
+
+            //Console.WriteLine("Após 1 mês, você terá R$ " + valorInvestido);
+            int contadorMes = 1;
+            while (contadorMes <= 12)
+            {
+                valorInvestido = valorInvestido + valorInvestido * 0.0036;
+
+                Console.WriteLine("Após " + contadorMes + " mês, você terá R$ " + valorInvestido.ToString("N2"));
+                contadorMes++;
+            }
+
+            //for (int i = 1; i <= 12; i++)
+            //{
+            //    valorInvestido = valorInvestido + valorInvestido * 0.0036;
+
+            //    Console.WriteLine("Após " + i + " mês, você terá R$ " + valorInvestido.ToString("N2"));
+            //}
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ P13-ForEncadeado/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace P13_ForEncadeado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Projeto 13");
+
+            // *
+            // **
+            // ***
+            for (int contadorLinha = 0; contadorLinha < 10; contadorLinha++)
+            {
+                for (int contadorColuna = 0; contadorColuna <= contadorLinha; contadorColuna++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            //Uma forma diferente 
+            for (int contadorLinha = 0; contadorLinha < 10; contadorLinha++)
+            {
+                for (int contadorColuna = 0; contadorColuna < 10; contadorColuna++)
+                {
+                    Console.Write("*");
+                    if (contadorColuna >= contadorLinha)
+                        break;
+
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
